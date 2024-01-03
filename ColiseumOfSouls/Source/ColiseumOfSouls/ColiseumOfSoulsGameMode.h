39b9c5cb30e9ab3e5370c6f3310a1e562989381b
KSIMAN <somnia.universum@gmail.com>
2024-01-04 01:02:08 +0300

    Первый коммит

---
+++ ColiseumOfSouls/Source/ColiseumOfSouls/ColiseumOfSoulsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ColiseumOfSoulsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AColiseumOfSoulsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AColiseumOfSoulsGameMode();
+};
+
+
+
